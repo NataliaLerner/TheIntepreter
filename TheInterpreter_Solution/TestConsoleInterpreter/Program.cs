6bf8d6728ae5f18c7c9a5c413b3623019bd821d6
HappyStore <coolman5811@hotmail.com>
2017-03-01 01:14:41 +0300

    Добавлены константы, изменены некоторые названия, вызывающую путаницу, устранены ошибки, протестированы методы

--- TheInterpreter_Solution/TestConsoleInterpreter/Program.cs
+++ TheInterpreter_Solution/TestConsoleInterpreter/Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections;
-using System.Linq;
-using System.Text;
-using System.IO;
 
-using Interpreter.Operations;
+using Interpreter.OperationCore;
 
 namespace TestConsoleInterpreter
 {
@@ -13,20 +8,19 @@ namespace TestConsoleInterpreter
     {
         static void Main(string[] args)
         {
-            Operations op = new Operations();
-
-            int x = 260555747;
+            OperationCore op = new OperationCore();
+            int num32 = 260555747;
             int y = 24;
             int newOp = 15;
 
-            x = op.SetOperation(x, y);
-            x = op.SetOperand(x, newOp, 1);
-            Console.WriteLine(op.GetOperand(x, 3));
-            Console.WriteLine(op.GetOperand(x, 2));
-            Console.WriteLine(op.GetOperand(x, 1));
-            Console.WriteLine(op.GetOperation(x));    
-            
-                    
+            num32 = op.SetOperation(num32, y);
+            num32 = op.SetRegister(num32, newOp, 1);
+            num32 = op.SetRegister(num32, newOp, 2);
+            num32 = op.SetRegister(num32, newOp, 3);
+            Console.WriteLine(op.GetRegister(num32, 3));
+            Console.WriteLine(op.GetRegister(num32, 2));
+            Console.WriteLine(op.GetRegister(num32, 1));
+            Console.WriteLine(op.GetOperation(num32));
         }
     }
 }
